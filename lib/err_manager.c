7b3a1806040bc372b1ad7fea65bcdd3e7e3528f2
nikigor <n.gorbadey@gmail.com>
2019-06-14 13:51:56 +0300

    error_manager: change display UI
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- lib/err_manager.c
+++ lib/err_manager.c
@@ -32,6 +32,7 @@ static void printf_config(void)
 void err_manager(void *arg)
 {
     (void) arg;
+    rtc_time_t rtc_time;
 
     oled_config();
     printf_config();
@@ -40,8 +41,27 @@ void err_manager(void *arg)
     xprintf("\n     Welcome to!\n\n");
     xprintf("  Smart Heat project\n");
     oled_update();
-    
+    vTaskDelay(1000);
+
     while (1) {
+
+        if (rtc_get_err_status() == RTC_DESYNCHRONIZATION) {
+            oled_clr(clBlack);
+            oled_set_cursor(0, 0);
+            xprintf ("\n     Attention!\n");
+            xprintf("   Need to update\n");
+            xprintf("    time and date");
+            oled_update();
+        }
+        else {
+            rtc_get_precise(&rtc_time);
+            oled_clr(clBlack);
+            oled_set_cursor(0, 0);
+            xprintf("Current time:\n");
+            xprintf("%d.%d.%d %d:%d:%d", rtc_time.date, rtc_time.month,
+                    rtc_time.year, rtc_time.hour, rtc_time.minute, rtc_time.second);
+            oled_update();
+        }
         LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
         vTaskDelay(1000);
     }
