9fa79801b6dd40736233cffc9bd56caf9c2a186c
nikigor <n.gorbadey@gmail.com>
2019-07-16 12:49:11 +0300

    user_interface: introduce display rendering
    1) Separate the screen for rtc and temperature displaying
    2) Make code neat
    
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- lib/user_interface.c
+++ lib/user_interface.c
@@ -1,10 +1,10 @@
 #include "user_interface.h"
 #include "oled_driver.h"
-#include "temperature.h"
 #include "xprintf.h"
-#include "rtc.h"
 
 #include "stm32f1xx_ll_gpio.h"
+
+static ui_ctrl_t ui_ctrl;
 
 /*
  * ----------------------------------------------------------------------------
@@ -22,6 +22,80 @@ static void printf_config(void)
 }
 
 /*
+ * Welcome page
+ */
+static void ui_welcome(void)
+{
+    oled_set_cursor(0, 0);
+    xprintf("\n     Welcome to!\n\n");
+    xprintf("  Smart Heat project\n");
+    oled_update();
+    vTaskDelay(1000);
+}
+/*
+ * Configure RTC message
+ */
+static void ui_rtc_show(void)
+{
+    oled_set_cursor(0, 0);
+    switch (ui_ctrl.rtc_err) {
+        case RTC_NO_ERR:
+            xprintf("Current time:\n");
+            xprintf("%02d.%02d.%d %02d:%02d:%02d", ui_ctrl.rtc_time.date,
+                    ui_ctrl.rtc_time.month, ui_ctrl.rtc_time.year,
+                    ui_ctrl.rtc_time.hour, ui_ctrl.rtc_time.minute,
+                    ui_ctrl.rtc_time.second);
+            break;
+        case RTC_WRONG_TIME:
+            xprintf("Error setting rtc: wrong format");
+            break;
+        case RTC_DESYNCHRONIZATION:
+            xprintf ("     Attention!\n");
+            xprintf("   Need to update\n");
+            xprintf("    time and date\n");
+            break;
+        case RTC_TOO_MUCH_NOTIFIERS:
+            xprintf("Too many notifiers\n");
+            break;
+        default:
+            xprintf("Error without handler oxxured!");
+            break;
+    }
+    return;
+}
+/*
+ * Configure temperature message
+ */
+static void ui_temp_show(void)
+{
+    int i = 0;
+    int temp_int = 0;
+    int temp_frac = 0;
+
+    oled_set_cursor(0, 3);
+    switch(ui_ctrl.temp_err) {
+        case TEMP_NO_ERR:
+            xprintf("Current temperature:\n");
+            for (i = 0; i < NUMBER_OF_TEMP_SNS; i++) {
+            temp_int = (int) ui_ctrl.temperature[i];
+            temp_frac = (int)((ui_ctrl.temperature[i] - (float)temp_int) * 100);
+            xprintf("%d.%d ", temp_int, temp_frac);
+            }
+            break;
+        default:
+            xprintf("Current temperature:\n");
+            for (i = 0; i < NUMBER_OF_TEMP_SNS; i++) {
+            temp_int = (int) ui_ctrl.temperature[i];
+            temp_frac = (int)((ui_ctrl.temperature[i] - (float)temp_int) * 100);
+            xprintf("%d.%d ", temp_int, temp_frac);
+            }
+            xprintf("\nSome errors occured!");
+            break;
+    }
+    return;
+}
+
+/*
  * ----------------------------------------------------------------------------
  * Public functions
  * ----------------------------------------------------------------------------
@@ -33,47 +107,32 @@ static void printf_config(void)
 void ui_manager(void *arg)
 {
     (void) arg;
-    rtc_time_t rtc_time;
-    int i = 0;
-    float temperature[NUMBER_OF_TEMP_SNS];
-    int temp_tmp;
 
     oled_config();
     printf_config();
     rtc_init();
-
-    xprintf("\n     Welcome to!\n\n");
-    xprintf("  Smart Heat project\n");
-    oled_update();
-    vTaskDelay(1000);
+    ui_welcome();
+    ui_ctrl.ui_screen_update = UI_SCREEN_UPDATE_MS;
 
     while (1) {
-
-        if (rtc_get_err_status() == RTC_DESYNCHRONIZATION) {
-            oled_clr(clBlack);
-            oled_set_cursor(0, 0);
-            xprintf ("     Attention!\n");
-            xprintf("   Need to update\n");
-            xprintf("    time and date\n");
-        }
-        else {
-            rtc_get_precise(&rtc_time);
-            oled_clr(clBlack);
-            oled_set_cursor(0, 0);
-            xprintf("Current time:\n");
-            xprintf("%d.%d.%d %d:%d:%d", rtc_time.date, rtc_time.month,
-                    rtc_time.year, rtc_time.hour, rtc_time.minute, rtc_time.second);
-        }
-        if (temp_get_err()) {
-            xprintf("Error occured\n");
-        }
-        temp_get_temp(temperature);
-        for (i = 0; i < NUMBER_OF_TEMP_SNS; i++) {
-            temp_tmp = (int)temperature[i];
-            xprintf("%d.%d ", temp_tmp, (int)((temperature[i] - temp_tmp) * 100));
-        }
+        /*
+         * Get errors and data from other modules
+         */
+        ui_ctrl.rtc_err = rtc_get_err_status();
+        rtc_get_precise(&ui_ctrl.rtc_time);
+        ui_ctrl.temp_err = temp_get_err();
+        temp_get_temp(ui_ctrl.temperature);
+        /*
+         * Show modules states
+         */
+        oled_clr(clBlack);
+        ui_rtc_show();
+        ui_temp_show();
         oled_update();
         LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
-        vTaskDelay(1000);
+        /*
+         * Go to block state
+         */
+        vTaskDelay(ui_ctrl.ui_screen_update);
     }
 }
--- lib/user_interface.h
+++ lib/user_interface.h
@@ -5,6 +5,22 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "rtc.h"
+#include "temperature.h"
+
+#define UI_SCREEN_UPDATE_MS         1000
+
+/*
+ * Typedef for user interface manager
+ */
+typedef struct {
+    rtc_err_t rtc_err;
+    temp_err_t temp_err;
+    rtc_time_t rtc_time;
+    float temperature[NUMBER_OF_TEMP_SNS];
+    uint32_t ui_screen_update;
+}ui_ctrl_t;
+
 /*
  * Memory for error manager task
  */
